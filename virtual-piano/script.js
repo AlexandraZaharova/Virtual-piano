c53247d00abe5a2dab48a0dafb070bcd57555a65
AlexandraZaharova <zakharova911@gmail.com>
2021-03-18 18:04:34 +0300

    feat: add toggle Notes/Letters

--- virtual-piano/script.js
+++ virtual-piano/script.js
@@ -1,5 +1,7 @@
 const piano = document.querySelector(".piano");
 const pianoButton = document.querySelectorAll(".piano-key");
+const btnContainer = document.querySelector(".btn-container");
+const btnNoteLet = document.querySelectorAll(".btn");
 
 function playAudio(src) {
     const audio = new Audio();
@@ -38,5 +40,23 @@ const stopCorrespondOver = (event) => {
     });
 }
 
+const activeBtn = (event) => {
+    if (!event.target.classList.contains("btn-active")) {
+        btnNoteLet.forEach((elem) => {
+            elem.classList.toggle("btn-active");
+        });
+    }
+    if (event.target.classList.contains("btn-letters")) {
+        pianoButton.forEach((elem) => {
+            elem.classList.add("piano-key-letter");
+        });
+    } else {
+        pianoButton.forEach((elem) => {
+            elem.classList.remove("piano-key-letter");
+        });
+    }
+}
+
 piano.addEventListener("mousedown", startCorrespondOver, false);
 piano.addEventListener("mouseup", stopCorrespondOver);
+btnContainer.addEventListener("click", activeBtn);
